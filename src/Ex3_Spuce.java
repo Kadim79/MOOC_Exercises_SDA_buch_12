f4a0d26116511ef3b0db7e290d6366558532459e
Kadim79 <cdimu@icloud.com>
2019-12-20 17:33:36 +0200

    First Commit - Week1 mooc.fi

---
+++ src/Ex3_Spuce.java
@@ -0,0 +1,27 @@
+public class Ex3_Spuce {
+
+    public static void main(String[] args) {
+
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *    ");
+        System.out.println(" o     o");
+        System.out.println("    l");
+        System.out.println();
+        System.out.println("   <>  ");
+    }
+
+}
+
+//    Create a program that prints the following:
+//
+//
+//         *
+//        ***
+//       *****
+//      *******
+//     *********
+//         *
---
+++ src/Ex4_VaryingVariables.java
@@ -0,0 +1,39 @@
+public class Ex4_VaryingVariables {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+
+//    The exercise file initially contains a program which prints:
+//
+//        Chickens:
+//        3
+//        Bacon (kg):
+//        5.5
+//        A tractor:
+//        There is none!
+//
+//        In a nutshell:
+//        3
+//        5.5
+//        There is none!
+//
+//        Change the program in the specified places so that it will print:
+//
+//        Chickens:
+//        9000
+//        Bacon (kg):
+//        0.1
+//        A tractor:
+//        Zetor
+//
+//        In a nutshell:
+//        9000
+//        0.1
+//        Zetor
+//
